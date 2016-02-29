9fa4246f4a26725fd4243e1ed63575a714088b7a
Bill Ulammandakh <bilguun.u@gmail.com>
2016-02-29 11:42:05 -0500

    Update ZIP_demographics_scraping.py

--- ZIP_demographics_scraping.py
+++ ZIP_demographics_scraping.py
@@ -1,7 +1,6 @@
 #! python3
 
-# A python script that scrapes ZIP code demographics attributes from the map on the ESRI ZIP Tapestry website
-# (http://www.esri.com/data/esri_data/ziptapestry) using the selenium module and writes the scraped results into
+# A python script that scrapes ZIP code demographics attributes from a website built on javascript, using the selenium module and writes the scraped results into
 # an Excel spreadsheet. Because the script is written to be used with the Firefox browser,
 # it is necessary to have Firefox installed to run the script.
 
